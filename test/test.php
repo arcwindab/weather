99b736fa1c8a6efb3657dcc44f015e6eaf316938
Tobias Jonson <tobias@arcwind.se>
2025-01-09 17:45:34 +0000

    Added test

--- test/test.php
+++ test/test.php
@@ -1,11 +1,22 @@
 <?php
-// Funktion för att addera två tal
-function add($a, $b) {
-    return $a + $b;
-}
 
-// Kontrollera om add-funktionen fungerar som förväntat
-if (add(2, 3) === 5) {
+require_once('../src/php/smhi.php');
+require_once('../src/php/weatherapi.php');
+require_once('../src/php/aurora.php');
+require_once('../src/php/kp.php');
+require_once('../src/php/place.php');
+
+$latitude            = 59.127241;
+$longitude           = 18.102768;
+
+
+$smhi = getWeatherDataSMHI($latitude, $longitude); // Fetch weather data from the SMHI service.
+$wd = getWeatherDataWeatherAPI( $latitude, $longitude, '');
+$place = getPlace($latitude, $longitude);
+$kp = getKPindex();
+$aurora = getAuroraProbability($latitude, $longitude);
+
+if ((!empty($smhi)) && ($place != '') && ($kp != '') && ($aurora != '')) {
     echo "Test passed!\n";
     exit(0); // Lyckas
 } else {
--- test/test.py
+++
@@ -1,6 +0,0 @@
-def add(a, b):
-    return a + b
-
-if __name__ == "__main__":
-    assert add(2, 3) == 5, "Test failed!"
-    print("Python test passed!")
